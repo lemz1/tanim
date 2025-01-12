8a1e5f189d8dcd5c2ee8c3245c293e9c26459e58
lemz1 <ismael.amjad07@gmail.com>
2025-01-12 23:27:40 +0100

    forgot to specify config

--- build-debug.bat
+++ build-debug.bat
@@ -1,2 +1,2 @@
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
-cmake --build build
+cmake --build build --config Debug
--- build-release.bat
+++ build-release.bat
@@ -1,2 +1,2 @@
 cmake -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build
+cmake --build build --config Release
