ad269046bdf93e6d55a10864cd54c2084c7e93af
lemz1 <ismael.amjad07@gmail.com>
2025-01-18 11:51:17 +0100

    msdfgen

--- build-debug.bat
+++ build-debug.bat
@@ -1,2 +1,2 @@
-cmake -B build -DCMAKE_BUILD_TYPE=Debug
+cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
 cmake --build build --config Debug
--- build-release.bat
+++ build-release.bat
@@ -1,2 +1,2 @@
-cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
 cmake --build build --config Release
