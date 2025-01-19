0f8b339209fef84190a0b09de1dc40521a046833
lemz1 <ismael.amjad07@gmail.com>
2025-01-19 15:05:45 +0100

    use static linking for vcpkg

--- build-release.bat
+++ build-release.bat
@@ -1,2 +1,2 @@
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake --build build --config Release
